73e416cb0f72d20327b9b9325367fb537096787d
abidali671 <abid.saeed.ali92@gmail.com>
2023-10-23 03:08:33 +0500

    update: base url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const clientAPI = Axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://quick-proposal.vercel.app/api",
   headers: {
     Authorization: "Bearer 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",
   },
