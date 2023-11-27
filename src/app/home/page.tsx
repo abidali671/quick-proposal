d9febc7002248e40b8fa6ac839677980df0d3ca2
Amir Ali <amir.saeed.ali92@gmail.com>
2023-11-27 16:29:14 +0500

    add fixed navbar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,74 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/global.css";
 
 const Home = () => {
+  const [stickyClass, setStickyClass] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("scroll", stickNavbar);
+
+    return () => {
+      window.removeEventListener("scroll", stickNavbar);
+    };
+  }, []);
+
+  const stickNavbar = () => {
+    if (window !== undefined) {
+      let windowHeight = window.scrollY;
+      windowHeight > 500
+        ? setStickyClass("bg-white shadow-md")
+        : setStickyClass("");
+    }
+  };
   return (
     <div>
+      <nav className={`hero-nav ${stickyClass}`}>
+        <div className="nav-container">
+          <Image
+            alt="logo"
+            width={110}
+            height={110}
+            
+            src={stickyClass ? 'https://n.foxdsgn.com/twilo/wp-content/uploads/2020/10/Group-113258899.png': "https://n.foxdsgn.com/twilo/wp-content/uploads/2020/10/Group-52-1.png"}
+          />
+          <ul
+            className={`${
+              !stickyClass ? "text-white " : "text-black "
+            } hero-ul`}
+          >
+            <li>Home</li>
+            <li>Pages</li>
+            <li>Portfolio</li>
+            <li>Blog</li>
+            <li>Shop</li>
+            <li>Contact</li>
+            <li>
+              <button className="primary-btn hidden md:block">
+                Get Started
+              </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-9 h-9 block md:hidden"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
+                />
+              </svg>
+            </li>
+          </ul>
+        </div>
+      </nav>
       {/* hero section start*/}
-      <div className="bg-gray-dark  min-h-screen  md:h-screen   relative">
+      <div className="bg-gray-dark relative">
         <div
           className="hero-overlay"
           style={{
@@ -15,28 +77,9 @@ const Home = () => {
           }}
         ></div>
         <div className="wrapper">
-          <nav className="hero-nav">
-            <Image
-              alt="logo"
-              width={110}
-              height={110}
-              src="https://n.foxdsgn.com/twilo/wp-content/uploads/2020/10/Group-52-1.png"
-            />
-            <ul className="hero-ul">
-              <li>Home</li>
-              <li>Pages</li>
-              <li>Portfolio</li>
-              <li>Blog</li>
-              <li>Shop</li>
-              <li>Contact</li>
-              <li>
-                <button className="primary-btn">Get Started</button>
-              </li>
-            </ul>
-          </nav>
           <div className="main-hero">
             <div className="hero-left">
-              <h1 className="text-6xl font-bold leading-[66px] text-white">
+              <h1 className="text-5xl font-bold leading-[60px] text-white">
                 Craft Compelling Proposals in{" "}
                 <span className="text-orange-light">
                   Minutes with Quick Proposal
