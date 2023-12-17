954f0aeda28cc7e835c442031f0b3ad2faa7bcb0
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-17 20:32:52 +0500

    update auth layout

--- src/app/(Auth)/layout.tsx
+++ src/app/(Auth)/layout.tsx
@@ -23,11 +23,11 @@ export default function RootLayout({
           <Header />
           <div className="relative flex items-center p-20">
             <img
-              className="left-0 top-0 absolute h-full w-full object-cover"
+              className="left-0 top-0 absolute h-full w-full object-cover grayscale"
               src={Background.src}
               alt="background"
             />
-            <div className="bg-white shadow p-5 relative w-full max-w-sm rounded-lg">
+            <div className="bg-gray-dark shadow p-5 relative w-full max-w-sm rounded-lg">
               {children}
             </div>
           </div>
