5a38bab970780a8e2f569f9744a2573175ac2318
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-18 11:51:58 +0500

    update user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,18 +1,20 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 interface IUser extends Document {
+  name: string;
   email: string;
   password: string;
 }
 
 const UserSchema = new Schema<IUser>(
   {
+    name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
 export default User;
