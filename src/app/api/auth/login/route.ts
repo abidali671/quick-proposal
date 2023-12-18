f3e6a8bbfd178f28d5c8e009f988320626219399
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-18 20:37:06 +0500

    add login api

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,15 +1,36 @@
-import User from "@/models/user";
-import connectMongoDB from "@/utils/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+import connectMongoDB from "@/utils/mongodb";
+import User from "@/models/user";
+import { ErrorHandler } from "@/utils";
 
 export async function POST(request: NextRequest) {
-  const { email, password } = await request.json();
+  try {
+    const { email, password } = await request.json();
+    await connectMongoDB();
 
-  await connectMongoDB();
-  await User.create({ email, password });
+    const user = await User.findOne({
+      email,
+    });
 
-  NextResponse.json(
-    { msg: `Verification mail sent to ${email}.` },
-    { status: 201 }
-  );
+    const isCorrectPassword =
+      user && (await bcrypt.compare(password, user.password));
+
+    if (isCorrectPassword) {
+      const { verified } = user;
+
+      if (!verified)
+        throw {
+          non_field_error: "User is not verified",
+        };
+    } else
+      throw {
+        non_field_error: "Invalid username or password",
+      };
+
+    return NextResponse.json({ msg: "Login Successful" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(ErrorHandler(error), { status: 500 });
+  }
 }
