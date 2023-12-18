a372c6e86373110705f00b32694c795b4b201b2d
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-18 20:59:43 +0500

    add error prop for input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,9 +2,15 @@ import React, { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  error?: string;
 }
 
-const Input: React.FC<InputProps> = ({ label, placeholder, ...rest }) => {
+const Input: React.FC<InputProps> = ({
+  label,
+  error,
+  placeholder,
+  ...rest
+}) => {
   return (
     <label className="flex flex-col gap-1">
       <p className="text-sm font-semibold">{label}</p>
@@ -13,6 +19,7 @@ const Input: React.FC<InputProps> = ({ label, placeholder, ...rest }) => {
         className="px-3 py-2 border-2 border-gray-500 rounded-lg"
         {...rest}
       />
+      {error && <p className="text-xs !text-red-500">{error}</p>}
     </label>
   );
 };
