23916dcadf8e92739642f669acf377741e419f4f
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-18 21:20:34 +0500

    update auth page layout

---
+++ src/components/AuthLayout.tsx
@@ -0,0 +1,20 @@
+import Background from "@/assets/auth-background.jpg";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex items-center p-5 md:p-20 max-md:justify-center">
+      <img
+        className="left-0 top-0 absolute h-full w-full object-cover grayscale"
+        src={Background.src}
+        alt="background"
+      />
+      <div className="bg-gray-dark shadow p-5 relative w-full max-w-sm rounded-lg">
+        {children}
+      </div>
+    </div>
+  );
+}
