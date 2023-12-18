a9607284f7a8025345131ed7f6eee3c3f8312a57
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-18 21:32:47 +0500

    update client base url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 const mongoUri =
   "mongodb+srv://abidali671:3qExTPaZjAJ6WX43@cluster0.cbh5wlu.mongodb.net/client?retryWrites=true&w=majority";
 
-const clientBaseUrl = "http://localhost:3000";
+const clientBaseUrl = "https://main.d1cy3q0nrgb450.amplifyapp.com";
 
 const mailerEmail = "abid.saeed.ali92@gmail.com";
 const mailerPassword = "zyti mjqw kshi cwkp";
