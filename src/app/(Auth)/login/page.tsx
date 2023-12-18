a3e8c5f017112a859ba67512f0a6cef64e4ca124
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-18 20:37:40 +0500

    add login page ui

--- src/app/(Auth)/login/page.tsx
+++ src/app/(Auth)/login/page.tsx
@@ -1,7 +1,55 @@
+"use client";
+
 import React from "react";
+import { useFormik } from "formik";
+import Input from "@/components/Input";
+import "./style.css";
+import Button from "@/components/Button";
+import { clientAPI } from "@/utils/api";
+import Link from "next/link";
 
 function Login() {
-  return <div>Login</div>;
+  const { values, setFieldValue, handleSubmit } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async (data) => {
+      const response = await clientAPI.post("/auth/login", data);
+    },
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1 className="font-bold text-2xl text-center text-white">Login</h1>
+      <p className="text-sm font-medium text-center text-gray-400">
+        Fill the form to continue
+      </p>
+      <div className="fields_wrapper">
+        <Input
+          label="Email"
+          value={values.email}
+          onChange={(event) => setFieldValue("email", event.target.value)}
+        />
+        <Input
+          label="Password"
+          value={values.password}
+          onChange={(event) => setFieldValue("password", event.target.value)}
+          type="password"
+        />
+        <Link className="text-gray-400 text-sm" href="/register">
+          Forgot password?
+        </Link>
+      </div>
+      <Button label="Continue" type="submit" />
+      <p className="text-white text-sm mt-2">
+        <span className="font-medium">Don't have an account?</span>{" "}
+        <Link className="text-gray-400" href="/register">
+          Register
+        </Link>
+      </p>
+    </form>
+  );
 }
 
 export default Login;
