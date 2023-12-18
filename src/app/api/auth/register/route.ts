15f086e99937cc8549220b10e365adfabb734e7e
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-18 20:36:24 +0500

    add register api

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,85 @@
+import { NextRequest, NextResponse } from "next/server";
+import optGenerator from "otp-generator";
+import nodemailer from "nodemailer";
+import bcrypt from "bcrypt";
+
+import { RegisterSchema } from "@/schema/auth";
+import connectMongoDB from "@/utils/mongodb";
+import config from "@/utils/config";
+import User from "@/models/user";
+import { ErrorHandler } from "@/utils";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { name, email, password } = await request.json();
+
+    await RegisterSchema.validate(
+      { name, email, password },
+      { abortEarly: false, strict: true }
+    );
+
+    await connectMongoDB();
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw { email: "Email already exists" };
+    }
+
+    const token = optGenerator.generate(12, {
+      upperCaseAlphabets: true,
+      lowerCaseAlphabets: true,
+      specialChars: false,
+    });
+
+    const bcryptPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      token,
+      password: bcryptPassword,
+      verified: false,
+    });
+
+    await RegisterMail({ id: user._id, email, name, token });
+
+    return NextResponse.json(
+      { msg: `Verification mail sent to ${email}.` },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(ErrorHandler(error), { status: 500 });
+  }
+}
+
+const nodeConfig = {
+  service: "gmail",
+  auth: {
+    user: config.mailerEmail,
+    pass: config.mailerPassword,
+  },
+};
+
+async function RegisterMail({ email, name, token, id }: any) {
+  try {
+    const transporter = nodemailer.createTransport(nodeConfig);
+    const verification_link = `${config.clientBaseUrl}/verify?id=${id}&token=${token}`;
+
+    await transporter.sendMail({
+      from: config.mailerEmail,
+      to: email,
+      subject: "Verify Your Account - Sign-Up Confirmation",
+      html: `<div><b>Dear ${name},</b>
+      <p>
+      To complete your sign-up, please click the verification link below:<br/>
+      <a target='_blank' href='${verification_link}'>${verification_link}</a></p>
+      <p>Thank you for joining Quick Proposal!</p>
+      <p>Cheers</p>
+      <p>Abid Ali</p>
+      </div>`,
+    });
+  } catch (error) {
+    throw error;
+  }
+}
