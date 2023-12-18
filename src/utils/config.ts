5276ee578e29f76b9e520a0e4857c219f1620ba5
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-18 11:51:05 +0500

    update mongo uri

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 const mongoUri =
-  "mongodb+srv://abidali671:3qExTPaZjAJ6WX43@cluster0.cbh5wlu.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://abidali671:3qExTPaZjAJ6WX43@cluster0.cbh5wlu.mongodb.net/client?retryWrites=true&w=majority";
 
 const config = {
   mongoUri,
