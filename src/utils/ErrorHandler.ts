a7b6dad2667dab6cc034edc9a2af7ced007d78ce
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-18 20:20:26 +0500

    add error handler

---
+++ src/utils/ErrorHandler.ts
@@ -0,0 +1,28 @@
+const ErrorHandler = (error: any) => {
+  let new_error: any = {};
+  if (error.code === 11000) {
+    // for mongoose already registered error
+    const error_key: any = [Object.keys(error.keyValue)[0]];
+    new_error = {
+      [error_key]: `This ${error_key} is already registered`,
+    };
+  } else if (error.name === "ValidationError") {
+    if (error.inner) {
+      console.log("error: ", error);
+      // for yup schema errors
+      error.inner.forEach(
+        (error: any) =>
+          (new_error[error.path || "non_field_error"] = error.message)
+      );
+    } else {
+      // for mongoose model schema errors
+      Object.keys(error.errors).map((key) => {
+        new_error[key] = error.errors[key].message;
+      });
+    }
+  } else return error;
+
+  return new_error;
+};
+
+export default ErrorHandler;
