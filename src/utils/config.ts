b10b5be8f8f47ffbb9ed87d2258c6e73a6b64639
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-18 22:57:56 +0500

    add env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 const mongoUri =
   "mongodb+srv://abidali671:3qExTPaZjAJ6WX43@cluster0.cbh5wlu.mongodb.net/client?retryWrites=true&w=majority";
 
-const clientBaseUrl = "https://main.d1cy3q0nrgb450.amplifyapp.com";
+const clientBaseUrl = process.env.NEXT_PUBLIC_CLIENT_BASE_URL;
 
 const mailerEmail = "abid.saeed.ali92@gmail.com";
 const mailerPassword = "zyti mjqw kshi cwkp";
