ef5de1dd75defda33a70963ecc107909fc550f8c
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-20 14:22:33 +0500

    remove prisma & mongoose

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,13 +1,9 @@
-const mongoUri =
-  "mongodb+srv://abidali671:3qExTPaZjAJ6WX43@cluster0.cbh5wlu.mongodb.net/client";
-
 const clientBaseUrl = process.env.NEXT_PUBLIC_CLIENT_BASE_URL;
 
 const mailerEmail = "abid.saeed.ali92@gmail.com";
 const mailerPassword = "zyti mjqw kshi cwkp";
 
 const config = {
-  mongoUri,
   mailerEmail,
   mailerPassword,
   clientBaseUrl,
--- src/utils/mongodb.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import config from "./config";
-
-const connectMongoDB = () => {
-  if (mongoose.connection.readyState >= 1) return;
-  mongoose.connect(config.mongoUri);
-};
-
-export default connectMongoDB;
