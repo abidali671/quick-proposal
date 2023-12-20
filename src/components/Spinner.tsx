e0294dea0e941bb24ea020669c4ce987c74826ed
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-21 00:01:45 +0500

    update spinner component

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-function Spinner() {
+function Spinner({ className }: { className?: string }) {
   return (
     <svg
       aria-hidden="true"
-      className="inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
+      className={
+        "inline w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300 " +
+        className
+      }
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
