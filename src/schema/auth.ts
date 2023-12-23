6203ad4c6eac891dfc490b5b46c8acc1d468a633
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-23 15:29:06 +0500

    add reset password schema

--- src/schema/auth.ts
+++ src/schema/auth.ts
@@ -1,10 +1,19 @@
 import { UnknownKey } from "@/utils";
-import { object, string } from "yup";
+import { object, string, ref } from "yup";
 
 export const RegisterSchema = object()
   .shape({
     name: string().required("Name is required"),
     email: string().required("Email is required"),
+    password: string().required("Password is required"),
+  })
+  .noUnknown(UnknownKey);
+
+export const ResetSchema = object()
+  .shape({
+    confirm_password: string()
+      .oneOf([ref("password")], "Passwords must match")
+      .required("Confirm password is required"),
     password: string().required("Password is required"),
   })
   .noUnknown(UnknownKey);
