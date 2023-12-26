6e83ceee971509f6619d120970b8ee339fb6ecd6
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-26 10:28:15 +0500

    update input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,55 @@
-import React, { InputHTMLAttributes } from "react";
+import React, { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+interface iInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: string;
 }
+
+interface iTextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label?: string;
+  error?: string;
+}
+
+type InputProps = { multiline?: boolean; variant?: "outlined" | "filled" } & (
+  | iInputProps
+  | iTextareaProps
+);
 
 const Input: React.FC<InputProps> = ({
   label,
   error,
   placeholder,
+  multiline,
+  className = "",
+  variant = "outlined",
   ...rest
 }) => {
   return (
-    <label className="flex flex-col gap-1">
-      <p className="text-sm font-semibold">{label}</p>
-      <input
-        placeholder={placeholder ?? label}
-        className="px-3 py-2 border-2 border-gray-500 rounded-lg"
-        {...rest}
-      />
+    <label className={"flex flex-col gap-1 " + className}>
+      {label && <p className="text-sm font-semibold">{label}</p>}
+      {multiline ? (
+        <textarea
+          placeholder={placeholder ?? label}
+          className={
+            "px-3 py-2 border-2 rounded-md " +
+            (variant === "outlined"
+              ? "border-gray-500"
+              : "border-gray-100 bg-gray-100")
+          }
+          {...(rest as iTextareaProps)}
+        />
+      ) : (
+        <input
+          placeholder={placeholder ?? label}
+          className={
+            "px-3 py-2 border-2 rounded-md " +
+            (variant === "outlined"
+              ? "border-gray-500"
+              : "border-gray-100 bg-gray-100")
+          }
+          {...(rest as iInputProps)}
+        />
+      )}
       {error && <p className="text-xs !text-red-500">{error}</p>}
     </label>
   );
