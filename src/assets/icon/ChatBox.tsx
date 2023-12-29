ceaa12aae982d15070bce0ee8f05ff354e4e55ba
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-29 11:36:30 +0500

    add icons

---
+++ src/assets/icon/ChatBox.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function ChatBox(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      stroke-width="0"
+      viewBox="0 0 512 512"
+      height="24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke-linejoin="round"
+        stroke-width="32"
+        d="M408 64H104a56.16 56.16 0 00-56 56v192a56.16 56.16 0 0056 56h40v80l93.72-78.14a8 8 0 015.13-1.86H408a56.16 56.16 0 0056-56V120a56.16 56.16 0 00-56-56z"
+      ></path>
+    </svg>
+  );
+}
+
+export default ChatBox;
--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -1,2 +1,4 @@
-export { default as LogoDark } from './logoDark'
-export { default as LogoLight } from './logoLight'
+export { default as ChatBox } from "./ChatBox";
+export { default as History } from "./History";
+export { default as LogoDark } from "./logoDark";
+export { default as LogoLight } from "./logoLight";
