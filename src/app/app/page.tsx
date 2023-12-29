8525372cb8b1cf0014c9619f904396deb5fa166c
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-29 11:53:01 +0500

    update app ui

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ChatBox, History } from "@/assets/icon";
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import { clientAPI } from "@/utils/api";
@@ -60,48 +61,70 @@ export default function Home() {
   };
 
   return (
-    <div className="w-full items-center grid grid-cols-[400px_1fr]">
-      <form
-        onSubmit={handleSend}
-        className=" bg-white w-full h-full flex flex-col gap-4 border-r border-gray-200 p-5 shadow-sm"
-      >
-        <Input
-          required
-          label="Job Title"
-          value={values.title}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            handleUpdateValue("title", e.target.value)
-          }
-        />
+    <div className="w-full grid md:grid-cols-[400px_1fr] max-md:grid-rows-[auto_1fr]">
+      <div className="md:grid md:grid-rows-[auto_auto_1fr] border-r border-gray-200 shadow-sm">
+        <form
+          onSubmit={handleSend}
+          className="bg-white grid gap-4 max-md:grid-cols-2 p-5"
+        >
+          <Input
+            required
+            label="Job Title"
+            value={values.title}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              handleUpdateValue("title", e.target.value)
+            }
+          />
 
-        <Input
-          rows={5}
-          multiline
-          required
-          label="Job Description"
-          value={values.description}
-          onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
-            handleUpdateValue("description", e.target.value)
-          }
-        />
+          <Input
+            label="Skills"
+            placeholder="Skills (optional)"
+            value={values.skills}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              handleUpdateValue("skills", e.target.value)
+            }
+          />
 
-        <Input
-          label="Skills"
-          placeholder="Skills (optional)"
-          value={values.skills}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            handleUpdateValue("skills", e.target.value)
-          }
-        />
+          <Input
+            rows={5}
+            multiline
+            required
+            className="max-md:h-[100px] max-md:col-span-2"
+            label="Job Description"
+            value={values.description}
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+              handleUpdateValue("description", e.target.value)
+            }
+          />
 
-        <Button
-          variant="secondary"
-          label="Generate Proposal"
-          type="submit"
-          loading={loading}
-        />
-      </form>
-      <div className="h-full bg-white w-full flex flex-col gap-2 p-5 relative ">
+          <Button
+            className="max-md:col-span-2"
+            variant="secondary"
+            label="Generate Proposal"
+            type="submit"
+            loading={loading}
+          />
+        </form>
+        <div className="border-b border-gray-200" />
+        <div className="p-5 flex flex-col gap-1 w-full max-md:hidden">
+          <div className="flex items-center gap-1 pb-1">
+            <History className="text-gray-500 h-4 w-4" />
+            <h6 className="text-xs font-semibold text-gray-400">HISTORY</h6>
+          </div>
+          {["Web Developer", "Mern Stack"].map((d, i) => (
+            <div
+              key={i}
+              className="grid grid-cols-[auto_1fr] gap-2 items-center bg-gray-50 hover:bg-gray-100 cursor-pointer px-5"
+            >
+              <ChatBox className="text-gray-500 w-5 h-5 min-w-5" />
+              <p className="text-sm leading-10 font-medium text-gray-600 truncate">
+                {d}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="h-full bg-white w-full flex flex-col gap-2 p-5 relative">
         <div className="h-full w-full border border-gray-200 p-5 relative">
           <div className="flex h-10 items-center justify-end absolute right-5 -top-5 bg-white rounded-md">
             {data.length > 0 && (
