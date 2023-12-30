8a8c9af3c66e56eee643683929ad57c7feba2d1d
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-31 00:07:37 +0500

    update authenticate error handler

--- src/utils/Authenticate.ts
+++ src/utils/Authenticate.ts
@@ -4,11 +4,12 @@ import config from "./config";
 const authenticate = (authToken: string) => {
   try {
     const token = authToken.split(" ")[1];
+
     const user = jwt.verify(token, config.secretAccessToken);
 
     return user;
   } catch (error) {
-    throw { auth: "Authentication failed" };
+    throw { auth: "Authentication failed", error };
   }
 };
 
