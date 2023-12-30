f022ade94ce4cbca60667121ce70c432bd42381c
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-30 23:29:28 +0500

    add jwt on login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 
-import { ErrorHandler, registerMail, supabase } from "@/utils";
+import { NextRequest, NextResponse } from "next/server";
+import { ErrorHandler, generateToken, registerMail, supabase } from "@/utils";
 
 export async function POST(request: NextRequest) {
   try {
@@ -37,7 +37,12 @@ export async function POST(request: NextRequest) {
         non_field_error: "Invalid username or password",
       };
 
-    return NextResponse.json({ msg: "Login Successful" }, { status: 201 });
+    delete user.password;
+    delete user.token;
+
+    const accessToken = generateToken(user);
+
+    return NextResponse.json({ accessToken, user }, { status: 201 });
   } catch (error) {
     return NextResponse.json(ErrorHandler(error), { status: 500 });
   }
