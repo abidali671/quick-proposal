912da53b971648949cec02f6dc0f1d709fd5894f
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-30 23:30:58 +0500

    add authenticate handler

---
+++ src/utils/Authenticate.ts
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import config from "./config";
+
+const authenticate = (authToken: string) => {
+  try {
+    const token = authToken.split(" ")[1];
+    const user = jwt.verify(token, config.secretAccessToken);
+
+    return user;
+  } catch (error) {
+    throw { auth: "Authentication failed" };
+  }
+};
+
+export default authenticate;
