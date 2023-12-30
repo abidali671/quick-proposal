99b15a2eaf6614c893bd8359231cffff54dc0c9a
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-31 00:14:57 +0500

    update protected route

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const router = useRouter();
-  const pathname = usePathname();
   const isLoggedIn = localStorage.getItem("isLoggedIn");
   if (isLoggedIn !== "true") {
-    router.push("/login?redirect" + pathname);
+    router.push("/login");
   }
 
   return children;
