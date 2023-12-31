86d0d57f6771838c14ad68dc4d4f70b658b275a7
abidali671 <abid.saeed.ali92@gmail.com>
2023-12-31 14:18:34 +0500

    add logout icon

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -2,3 +2,4 @@ export { default as ChatBox } from "./ChatBox";
 export { default as History } from "./History";
 export { default as LogoDark } from "./logoDark";
 export { default as LogoLight } from "./logoLight";
+export { default as Logout } from "./Logout";
