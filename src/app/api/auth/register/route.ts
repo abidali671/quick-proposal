6057afeae0c5dc464120a1a89f9843c75783524b
abidali671 <abid.saeed.ali92@gmail.com>
2024-06-04 12:34:47 +0500

    update register api

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -3,25 +3,24 @@ import optGenerator from "otp-generator";
 import bcrypt from "bcrypt";
 
 import { RegisterSchema } from "@/schema/auth";
-import { ErrorHandler, registerMail, supabase } from "@/utils";
+import { ErrorHandler, registerMail } from "@/utils";
 import config from "@/utils/config";
+import UserModel from "@/model/User";
+import ConnectDB from "@/lib/ConnectDB";
 
 export async function POST(request: NextRequest) {
   try {
+    await ConnectDB();
     const { name, email, password } = await request.json();
 
     await RegisterSchema.validate(
       { name, email, password },
       { abortEarly: false, strict: true }
     );
+    const existingUserByEmail = await UserModel.findOne({ email });
 
-    let { data: existingUserByEmail } = await supabase
-      .from("Users")
-      .select("*")
-      .eq("email", email);
-
-    if (existingUserByEmail?.length) {
-      throw { email: "Email already registered" };
+    if (existingUserByEmail) {
+      throw { email: "Email already registered", existingUserByEmail };
     }
 
     const token = optGenerator.generate(12, {
@@ -32,24 +31,18 @@ export async function POST(request: NextRequest) {
 
     const bcryptPassword = await bcrypt.hash(password, 10);
 
-    const { data, error } = await supabase
-      .from("Users")
-      .insert([
-        {
-          name,
-          email,
-          token,
-          verified: false,
-          password: bcryptPassword,
-          credits: config.initialFreeCredits,
-        },
-      ])
-      .select();
+    const user = new UserModel({
+      name,
+      email,
+      token,
+      verified: false,
+      password: bcryptPassword,
+      credits: config.initialFreeCredits,
+    });
 
-    if (error)
-      throw { msg: "Something is wrong. Please try again later", error };
+    await user.save();
 
-    await registerMail({ id: data[0].id, email, name, token });
+    await registerMail({ id: user._id, email, name, token });
 
     return NextResponse.json(
       { msg: `Verification link sent to: ${email}` },
