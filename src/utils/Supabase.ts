642c2f3cc1679dda2410a854d04e292ce5a95b6b
abidali671 <abid.saeed.ali92@gmail.com>
2024-06-06 12:03:35 +0500

    remove supabase

--- src/utils/Supabase.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://rswqowhmjbwycxzefpop.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzd3Fvd2htamJ3eWN4emVmcG9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwNjAyNTIsImV4cCI6MjAxODYzNjI1Mn0.VZeOAslkP3XHYBg0ircAw7jWmjOaN_ZpVMuMnvbdl_4";
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,5 @@ export { default as API } from "./api";
 export { default as ErrorHandler } from "./ErrorHandler";
 export { default as registerMail } from "./RegisterMail";
 export { default as resetMail } from "./ResetMail";
-export { default as supabase } from "./Supabase";
 export { default as generateToken } from "./Token";
 export { default as UnknownKey } from "./UnknownKey";
